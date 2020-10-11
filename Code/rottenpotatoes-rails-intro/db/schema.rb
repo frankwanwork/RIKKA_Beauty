4f298ccdadee896f190452184e35b061128581ae
Jianwei Huang <hjw@tamu.edu>
2020-10-11 10:58:08 -0500

    update schema

--- Code/rottenpotatoes-rails-intro/db/schema.rb
+++ Code/rottenpotatoes-rails-intro/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150809022253) do
+ActiveRecord::Schema.define(version: 20201011152934) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(version: 20150809022253) do
     t.datetime "release_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string  "email"
+    t.string  "username"
+    t.string  "password"
+    t.integer "type"
+    t.string  "lastname"
+    t.string  "firstname"
+    t.string  "phone"
   end
 
 end
