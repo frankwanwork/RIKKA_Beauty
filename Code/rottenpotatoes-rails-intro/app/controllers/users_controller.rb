2ef5d1c278a4a427bd5135602d983042ed10cd01
EC2 Default User <ec2-user@ip-172-31-6-193.ec2.internal>
2020-10-12 21:57:34 +0000

    users contoller updates lynn

--- Code/rottenpotatoes-rails-intro/app/controllers/users_controller.rb
+++ Code/rottenpotatoes-rails-intro/app/controllers/users_controller.rb
@@ -1,42 +1,59 @@
 class UsersController < ApplicationController
 
-  # def movie_params
-  #   params.require(:movie).permit(:title, :rating, :description, :release_date)
-  # end
+  private 
+  def user_params
+     params.require(:user).permit(:email, :username, :password, :type, :lastname, :firstname, :phone)
+  end
 
   def show
-    id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
+    #id = params[:id] # retrieve movie ID from URI route
+    #@movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
 
   def new
-    # create user
+    # create user instance
     # Note: should check the security code
+    @user = User.new
   end
 
   def signin_show
     # signin homepage
-    @movies = Movie.all
+    #@movies = Movie.all
   end
 
   def signin
     # sign in
-    @movie = Movie.create!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
+    @user = User.find_by(username: params[:username], password: params[:password])
+    
+    if @user
+      session[:user_name] = @user.username
+      flash[:notice] = ' user#{@user.username} logins in successfully!'
+      # also did not redirect to the different page according to the user type
+      redirect_to root1_path # haven't created yet
+    else
+      flash[:notice] = ' the username or password is not correct'
+      redirect_to signin_users_path
+    end
   end
 
   def signup_show
     # signup homepage
     @movies = Movie.all
+    
   end
 
   def signup
     # sign up
-    @movie = Movie.create!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
+    if params[:magicCode] == "rikka"
+      params[:type] = 1
+    else
+      params[:type] = 0
+    end
+    
+    @user = User.create!(user_params)
+    flash[:notice] = "#{@user.username} was successfully created."
+    redirect_to signin_users_path
   end
 
   def forgot_show
@@ -46,13 +63,12 @@ class UsersController < ApplicationController
 
   def forgot
     # forgot password
-    @movie = Movie.create!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
+    @user = User.find params[:username]
+    @user.update_attributes!(user_params[:password])
+    flash[:notice] = "#{@user.username} was successfully updated."
+    redirect_to signin_users_path
   end
 
   def send_code
     # send verfication code to the email
   end
-
-end
