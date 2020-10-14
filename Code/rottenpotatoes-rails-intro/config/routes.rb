2e850ebdac370ffda7584384d830c6c2543a9e52
Jianwei Huang <hjw@tamu.edu>
2020-10-13 19:55:27 -0500

    fix: correct the route for signup

--- Code/rottenpotatoes-rails-intro/config/routes.rb
+++ Code/rottenpotatoes-rails-intro/config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
       match 'signin', to: 'users#signin_show', via: [:get]
       match 'signin', to: 'users#signin', via: [:post]
       match 'signup', to: 'users#signup_show', via: [:get]
-      match 'signup', to: 'users#new', via: [:post]
+      match 'signup', to: 'users#signup', via: [:post]
       match 'forgot', to: 'users#forgot_show', via: [:get]
       match 'forgot', to: 'users#forgot', via: [:post]
       match 'send_code', to: 'users#send_code', via: [:get]
