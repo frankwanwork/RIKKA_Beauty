f1bd3a3f97ce2022d5109b89628f0cfdaf7b8266
EC2 Default User <ec2-user@ip-172-31-34-109.us-east-2.compute.internal>
2020-10-26 05:53:34 +0000

    iter2

--- Code/db/schema.rb
+++ Code/db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201025203143) do
+ActiveRecord::Schema.define(version: 20201026050606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "image"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "pictures", force: :cascade do |t|
     t.string   "productName"
