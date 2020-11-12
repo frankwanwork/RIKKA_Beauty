d9e60d401f2a3233617b4b7bb2da4b5cbf7103b4
Lynn <qhl-96@tamu.edu>
2020-11-12 00:36:30 +0000

    appointment controller created

---
+++ Code/app/controllers/appointments_controller.rb
@@ -0,0 +1,13 @@
+class AppointmentsController < ApplicationController
+  def show
+  end
+
+  def add
+  end
+
+  def edit
+  end
+
+  def drop
+  end
+end
--- Code/db/schema.rb
+++ Code/db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201109220426) do
+ActiveRecord::Schema.define(version: 20201112000438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.date     "appt_date"
+    t.string   "timeslot"
+    t.string   "user_name"
+    t.string   "user_tele"
+    t.string   "user_email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pictures", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -27,7 +37,6 @@ ActiveRecord::Schema.define(version: 20201109220426) do
   create_table "products", force: :cascade do |t|
     t.string "productname"
     t.text   "description"
-    t.string "tags"
     t.string "pics"
     t.string "price"
   end
@@ -41,13 +50,32 @@ ActiveRecord::Schema.define(version: 20201109220426) do
     t.datetime "updated_at"
   end
 
-  create_table "tags", force: :cascade do |t|
-    t.string   "tagName"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context",       limit: 128
+    t.datetime "created_at"
   end
 
-  add_index "tags", ["tagName"], name: "index_tags_on_tagName", unique: true, using: :btree
+  add_index "taggings", ["context"], name: "index_taggings_on_context", using: :btree
+  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
+  add_index "taggings", ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy", using: :btree
+  add_index "taggings", ["taggable_id"], name: "index_taggings_on_taggable_id", using: :btree
+  add_index "taggings", ["taggable_type"], name: "index_taggings_on_taggable_type", using: :btree
+  add_index "taggings", ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type", using: :btree
+  add_index "taggings", ["tagger_id"], name: "index_taggings_on_tagger_id", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string  "name"
+    t.integer "taggings_count", default: 0
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string  "email"
